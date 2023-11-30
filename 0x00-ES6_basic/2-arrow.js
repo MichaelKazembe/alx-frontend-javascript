303fe9e27c7c4b2d3e6683568f9027034a76414c
MichaelKazembe <michaelkazembe@gmail.com>
2023-11-30 10:12:09 +0000

    Arrow Function

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+
+export default function taskBlock(trueOrFalse) {
+	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+	const self = this;
+	this.addNeighborhood = (newNeighborhood) => {
+		self.sanFranciscoNeighborhoods.push(newNeighborhood);
+		return self.sanFranciscoNeighborhoods;
+	};
+}
